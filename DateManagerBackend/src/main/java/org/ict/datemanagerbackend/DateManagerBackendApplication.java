4d9dd5e4226e217a499ad2d055af65f06ca55955
kimhanee2021-ux <kimhanee2021@gmail.com>
2026-08-04 18:22:54 +0900

    준비작업

---
+++ DateManagerBackend/src/main/java/org/ict/datemanagerbackend/DateManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package org.ict.datemanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateManagerBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DateManagerBackendApplication.class, args);
+  }
+
+}
---
+++ DateManagerBackend/src/test/java/org/ict/datemanagerbackend/DateManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ict.datemanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateManagerBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
