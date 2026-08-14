15c65b34abdd52fd6dd0ce06ac2aa9736c36775c
kimhan <chickenleg3014@gmail.com>
2026-08-14 20:16:03 +0900

    .
    .

---
+++ DateManagerBackend/src/main/java/org/ict/datemanagerbackend/domain/place/service/PlaceService.java
@@ -0,0 +1,10 @@
+package org.ict.datemanagerbackend.domain.place.service;
+
+
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@NoArgsConstructor
+public class PlaceService {
+}
